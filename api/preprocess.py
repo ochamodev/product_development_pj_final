1c7d17e64160758947cee8a9a8c8abddaece6678
marianvela <marian.vela@galileo.edu>
2024-12-01 00:20:31 -0600

    fix: preprocess normalization

--- api/preprocess.py
+++ api/preprocess.py
@@ -13,6 +13,7 @@ async def preprocess_image(image_file):
     transform = transforms.Compose([
         transforms.Resize((224, 224)),  # Resize to 224x224
         transforms.ToTensor(),          # Convert to tensor and normalize to [0, 1]
+        transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
     ])
     image_tensor = transform(image).unsqueeze(0)  # Add batch dimension
 
