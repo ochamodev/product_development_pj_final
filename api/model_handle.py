2fb656ad7bac2b676ac9eae54619a3c57512e3a1
ochamo <cheleyotto98@gmail.com>
2024-12-01 00:13:09 -0600

    feat: update dictionary

--- api/model_handle.py
+++ api/model_handle.py
@@ -2,9 +2,9 @@
 MODEL HANDLER
 '''
 
-import tensorflow as tf
 import torch
 import numpy as np
+
 
 # Load TensorFlow/Keras models
 keras_model1 = tf.keras.models.load_model("../model/fine_tune_resnet.keras")
@@ -16,46 +16,46 @@ pt_model.eval()
 
 # Subcategory and category mapping
 CLASS_MAPPING = {
-    1: {"categoria": "Inorgánico", "nombre": "Aerosoles"},
-    2: {"categoria": "Inorgánico", "nombre": "Latas de comida de aluminio"},
-    3: {"categoria": "Inorgánico", "nombre": "Latas de refresco de aluminio"},
-    4: {"categoria": "Orgánico", "nombre": "Cajas de cartón"},
-    5: {"categoria": "Orgánico", "nombre": "Empaques de cartón"},
-    6: {"categoria": "Inorgánico", "nombre": "Ropa"},
-    7: {"categoria": "Orgánico", "nombre": "Residuos de café"},
-    8: {"categoria": "Inorgánico", "nombre": "Cubiertos de plástico desechables"},
-    9: {"categoria": "Orgánico", "nombre": "Cáscaras de huevo"},
-    10: {"categoria": "Orgánico", "nombre": "Residuos de comida"},
-    11: {"categoria": "Inorgánico", "nombre": "Botellas de vidrio para bebidas"},
-    12: {"categoria": "Inorgánico", "nombre": "Envases cosméticos de vidrio"},
-    13: {"categoria": "Inorgánico", "nombre": "Frascos de vidrio para alimentos"},
-    14: {"categoria": "Orgánico", "nombre": "Revistas"},
-    15: {"categoria": "Orgánico", "nombre": "Periódicos"},
-    16: {"categoria": "Orgánico", "nombre": "Papel de oficina"},
-    17: {"categoria": "Orgánico", "nombre": "Tazas de papel"},
-    18: {"categoria": "Inorgánico", "nombre": "Tapas de plástico para tazas"},
-    19: {"categoria": "Inorgánico", "nombre": "Botellas de detergente de plástico"},
-    20: {"categoria": "Inorgánico", "nombre": "Envases de plástico para alimentos"},
-    21: {"categoria": "Inorgánico", "nombre": "Bolsas de compras de plástico"},
-    22: {"categoria": "Inorgánico", "nombre": "Botellas de refresco de plástico"},
-    23: {"categoria": "Inorgánico", "nombre": "Popotes de plástico"},
-    24: {"categoria": "Inorgánico", "nombre": "Bolsas de basura de plástico"},
-    25: {"categoria": "Inorgánico", "nombre": "Botellas de agua de plástico"},
-    26: {"categoria": "Inorgánico", "nombre": "Zapatos"},
-    27: {"categoria": "Inorgánico", "nombre": "Latas de comida de acero"},
-    28: {"categoria": "Inorgánico", "nombre": "Tazas de unicel (poliestireno)"},
-    29: {"categoria": "Inorgánico", "nombre": "Envases de comida de unicel (poliestireno)"},
-    30: {"categoria": "Orgánico", "nombre": "Bolsas de té"}
+    1: {"classification": "Inorgánico", "type_of_material": "Aerosoles"},
+    2: {"classification": "Inorgánico", "type_of_material": "Latas de comida de aluminio"},
+    3: {"classification": "Inorgánico", "type_of_material": "Latas de refresco de aluminio"},
+    4: {"classification": "Orgánico", "type_of_material": "Cajas de cartón"},
+    5: {"classification": "Orgánico", "type_of_material": "Empaques de cartón"},
+    6: {"classification": "Inorgánico", "type_of_material": "Ropa"},
+    7: {"classification": "Orgánico", "type_of_material": "Residuos de café"},
+    8: {"classification": "Inorgánico", "type_of_material": "Cubiertos de plástico desechables"},
+    9: {"classification": "Orgánico", "type_of_material": "Cáscaras de huevo"},
+    10: {"classification": "Orgánico", "type_of_material": "Residuos de comida"},
+    11: {"classification": "Inorgánico", "type_of_material": "Botellas de vidrio para bebidas"},
+    12: {"classification": "Inorgánico", "type_of_material": "Envases cosméticos de vidrio"},
+    13: {"classification": "Inorgánico", "type_of_material": "Frascos de vidrio para alimentos"},
+    14: {"classification": "Orgánico", "type_of_material": "Revistas"},
+    15: {"classification": "Orgánico", "type_of_material": "Periódicos"},
+    16: {"classification": "Orgánico", "type_of_material": "Papel de oficina"},
+    17: {"classification": "Orgánico", "type_of_material": "Tazas de papel"},
+    18: {"classification": "Inorgánico", "type_of_material": "Tapas de plástico para tazas"},
+    19: {"classification": "Inorgánico", "type_of_material": "Botellas de detergente de plástico"},
+    20: {"classification": "Inorgánico", "type_of_material": "Envases de plástico para alimentos"},
+    21: {"classification": "Inorgánico", "type_of_material": "Bolsas de compras de plástico"},
+    22: {"classification": "Inorgánico", "type_of_material": "Botellas de refresco de plástico"},
+    23: {"classification": "Inorgánico", "type_of_material": "Popotes de plástico"},
+    24: {"classification": "Inorgánico", "type_of_material": "Bolsas de basura de plástico"},
+    25: {"classification": "Inorgánico", "type_of_material": "Botellas de agua de plástico"},
+    26: {"classification": "Inorgánico", "type_of_material": "Zapatos"},
+    27: {"classification": "Inorgánico", "type_of_material": "Latas de comida de acero"},
+    28: {"classification": "Inorgánico", "type_of_material": "Tazas de unicel (poliestireno)"},
+    29: {"classification": "Inorgánico", "type_of_material": "Envases de comida de unicel (poliestireno)"},
+    30: {"classification": "Orgánico", "type_of_material": "Bolsas de té"}
 }
 
 def map_to_category(class_id):
     mapping = CLASS_MAPPING.get(class_id)
     if mapping is None:
-        return {"nombre": "Basura", "categoria": "Inorgánico"}
+        return {"type_of_material": "Basura", "classification": "Inorgánico"}
     
     return {
-        "nombre": mapping["nombre"], 
-        "categoria": mapping["categoria"]
+        "type_of_material": mapping["type_of_material"], 
+        "classification": mapping["classification"]
     }
     
 def predict(image_tensor):
